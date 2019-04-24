d17a6b9f9819e45c36b2744144df3e7b2bfc947b
virmon <virmontumulak@gmail.com>
2019-04-24 14:13:05 +0800

    feat: integrate stack navigation

--- App.js
+++ App.js
@@ -1,18 +1,99 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Platform, StatusBar } from 'react-native'
 import AddEntry from './components/AddEntry'
 import History from './components/History'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import { 
+  createAppContainer, 
+  createBottomTabNavigator, 
+  createMaterialTopTabNavigator, 
+  createStackNavigator 
+} from 'react-navigation'
+import { purple, white } from './utils/colors'
+import { Fontawesome, Ionicons } from '@expo/vector-icons'
+import { Constants } from 'expo'
+import EntryDetail from './components/EntryDetail'
+
+function UdaciStatusBar ({ backgroundColor, ...props }) {
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+
+
+const tabs = {
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarLabel: 'History',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    },
+  },
+  AddEntry: {
+    screen: AddEntry,
+    navigationOptions: {
+      tabBarLabel: 'Add Entry',
+      tabBarIcon: ({ tintColor }) => <Fontawesome name='plus-square' size={30} color={tintColor} />
+    },
+  },
+}
+
+const options = {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgb(0, 0, 0, 0.24)',
+      shadowOffsetL: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+}
+
+const Nav = createAppContainer(Platform.OS === 'ios' 
+                                  ? createBottomTabNavigator(tabs, options) 
+                                  : createMaterialTopTabNavigator(tabs, options))
+
+const StackNavigator = createStackNavigator({
+  Home: {
+    screen: Nav,
+    navigationOptions: {
+      header: null
+    } 
+  },
+  EntryDetail: {
+    screen: EntryDetail,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple,
+      }
+    }
+  }
+})
+
+const MainNavigator = createAppContainer(StackNavigator)
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex:1}}>
-          <View style={{height:20}} />
-          <History />
+          <UdaciStatusBar backgroundColor={purple} barStyle='light-content' />
+          <MainNavigator />
         </View>
       </Provider>
     );
--- components/AddEntry.js
+++ components/AddEntry.js
@@ -9,7 +9,8 @@ import TextButton from './TextButton'
 import { submitEntry, removeEntry } from '../utils/api'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
-import { white, purple } from '../utils/colors';
+import { white, purple } from '../utils/colors'
+import { NavigationActions } from 'react-navigation'
 
 function SubmitBtn ({ onPress }) {
     return (
@@ -73,7 +74,7 @@ class AddEntry extends Component {
             eat: 0,
         }))
 
-        // Navigate to home
+        this.toHome()
 
         submitEntry({ key, entry })
 
@@ -86,9 +87,14 @@ class AddEntry extends Component {
             [key]: getDailyReminderValue()
         }))
 
-        // Route to Home
+        this.toHome()
 
         removeEntry(key)
+    }
+    toHome = () => {
+        this.props.navigation.dispatch(NavigationActions.back({
+            key: 'AddEntry'
+        }))
     }
     render () {
         const metaInfo = getMetricMetaInfo()
